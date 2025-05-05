2135c2d9b56b74188495ea67468649ac3edc030f
athimjon <tonym5one@gmail.com>
2025-05-05 15:21:45 +0500

    Initial Role insert added .

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,9 @@
--- Create sequence
+-- Create sequence only if it doesn't exist
 CREATE SEQUENCE IF NOT EXISTS position_number_seq START 1;
 
--- Create function
+-- Create function only if it doesn't exist
 CREATE OR REPLACE FUNCTION set_position_number()
-    RETURNS TRIGGER AS $$
+    RETURNS trigger AS $$
 BEGIN
     IF NEW.position_number IS NULL THEN
         NEW.position_number := nextval('position_number_seq');
@@ -12,8 +12,18 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Create trigger
-CREATE TRIGGER IF NOT EXISTS position_number_trigger
-    BEFORE INSERT ON status
-    FOR EACH ROW
-EXECUTE FUNCTION set_position_number();
+-- Create trigger only if it doesn't exist
+DO
+$$
+    BEGIN
+        IF NOT EXISTS (SELECT 1
+                       FROM pg_trigger
+                       WHERE tgname = 'position_number_trigger') THEN
+            CREATE TRIGGER position_number_trigger
+                BEFORE INSERT
+                ON status
+                FOR EACH ROW
+            EXECUTE FUNCTION set_position_number();
+        END IF;
+    END
+$$;
